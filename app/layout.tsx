fd1e95e21f1526e815e66416d804d2253d9817ec
Taliesin Sisson <taliesins@yahoo.com>
2025-05-31 16:35:01 +0000

    Its alive

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+// app/layout.tsx
+import React from 'react';
+
+export const metadata = {
+  title: 'declare-cloud/website',
+  description: 'A Next.js app running on Bun!',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
