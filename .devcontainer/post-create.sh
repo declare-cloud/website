d6962b3e66a6febf024468a504bef0b3959c5c14
ebru <ebrudev@bridgelabsai.com>
2025-06-04 18:41:32 +0000

    fix: adding `/workspace` directory as a safe.directory for git refs: #9

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -16,3 +16,10 @@ bun install
 bun next telemetry disable
 bun add next react react-dom
 bunx commitizen init cz-conventional-changelog --save-dev --save-exact
+
+if [[ -d /workspace/.git ]]; then
+  echo "Setting safe.directory for git..."
+  git config --global --add safe.directory /workspace || echo "git config safe.directory failed, but continuing..."
+else
+  echo "No .git directory found in /workspace, skipping git safe.directory configuration."
+fi
